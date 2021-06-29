434bce6b4c22fa78c0b14aca1cdea3bb369595ad
Jonathan S <jonastrijker@gmail.com>
2021-06-29 18:53:38 +0200

    Bezig aanpassen plotYears

--- Visualization/plotYears.py
+++ Visualization/plotYears.py
@@ -10,25 +10,33 @@ folder = cwd + "/DataSets/OpenDataUitspraken"
 for file in os.listdir(folder) : # Loop door elke file in de folder en stop het jaartal uit de titel in een list
     years.append(file.split('_')[3])
 
+def menu () :
+    print("Please enter year range to plot. Default = ")
+    var = input('\nEnter action: ')
+    # Iets
+    # Also print frequencies in numbers?
+
 def frequencies () :
-    temp = Counter(years)
-    # temp = Counter(years).most_common() # Sort most common
+    temp = Counter(years) # Counter(years).most_common() # Sort most common
+    # Menu(1911,2021)
     temp2 = dict(temp)
-    print(temp2[0] + ':' + temp2[-1])
-    print(temp2) # Print frequencies of court decision per year
+    #print(temp2.items[:1])
+    #print(temp2) # Print frequencies of court decision per year
     count = 0
     for i in temp2 :
         if (int(i) <= 1993) :
             count+=temp2[i]
-    print(count) # Court decisions, years 1911-1993
+    # print(count) # Court decisions, years 1911-1993
 
-years.sort() # Sort years ascending
-plt.xlabel("Year")
-plt.ylabel("Court decisions")
-plt.xticks(rotation=90)
-years_set = set(years)
-years_bins = len(years_set) # Get unique year numbers
-plt.hist(years, bins = years_bins)
-plt.show()
+def plot () :
+    years.sort() # Sort years ascending
+    plt.xlabel("Year")
+    plt.ylabel("Court decisions")
+    plt.xticks(rotation=90)
+    years_set = set(years)
+    years_bins = len(years_set) # Get unique year numbers
+    plt.hist(years, bins = years_bins)
+    plt.show()
 
+#plot()
 frequencies()
