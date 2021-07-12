06081145966fbf0d55ea1785fd3d9d2b3ad63dbb
Jonathan S <jonastrijker@gmail.com>
2021-07-12 05:48:09 +0200

    gdfgdfgdf

--- Scripts/CSV_scripts/check_anchor_text.py
+++ Scripts/CSV_scripts/check_anchor_text.py
@@ -9,6 +9,7 @@ def main () :
     else_count = 0
     newLine_count = 0
     anchors = []
+    courts = []
     with open(file, 'r') as total :
         csv_reader = reader(total)
         next(csv_reader) # Skip header
@@ -17,13 +18,16 @@ def main () :
             anchors.append(anchor)
             if (anchor == "") :
                 empty_count+=1
-            elif (anchor == "Cichowski") :
+            elif (anchor == "Muller") :
+                court = row[0].split(":")[2]
+                courts.append(court)
                 print(row[0],row[1],row[2])
             else :
                 else_count+=1
     print("Empty anchor text: ",empty_count)
     print("New line count: ",newLine_count)
     print("Else: ",else_count)
-    print('Frequencies: ',Counter(anchors).most_common(100))
+    print('Frequencies courts: ',Counter(courts).most_common())
+    #print('Frequencies: ',Counter(anchors).most_common(100))
 
 main()
--- Scripts/CSV_scripts/check_uri.py
+++ Scripts/CSV_scripts/check_uri.py
@@ -12,8 +12,8 @@ def writeToCSV (rows) :
         with open(csvFile, 'w') as csvOpen: 
             csvWriter = csv.writer(csvOpen) 
             csvWriter.writerow(fields) 
-            for i in rows :
-                csvWriter.writerows(i)
+            for row in rows :
+                csvWriter.writerows([row])
 
 def printStats(normal_count,exclamationMark_count,multiple_count1,multiple_count2,empty_count,tilde_count,other_count,total_count,else_count) :
     print("Normal ECLI count: ",normal_count)
@@ -39,6 +39,8 @@ def court_type (court) :
 def main () :
     file = os.getcwd() + "/CSV/Total.csv"
     courts = []
+
+    printList = []
     new_rows = []
     normal_count = 0
     exclamationMark_count = 0
@@ -49,7 +51,6 @@ def main () :
     other_count = 0
     total_count = 0
     else_count = 0
-    tildes = []
 
     with open(file, 'r') as total :
         csv_reader = reader(total)
@@ -59,17 +60,18 @@ def main () :
             court = row[0].split(':')[2]
             courts.append(court_type(court))
             target = row[1]
+            printList.append(row[2])
             if (target == '!') :
                 exclamationMark_count+=1
             elif (target == "") :
                 empty_count+=1
             elif (target.endswith("~")) :
-                print(row[0],target,row[2])
-                tildes.append(row[2])
+                #print(row[0],target,row[2])
                 tilde_count+=1
             elif ("ECLI" not in target or "<" in target) :
                 other_count+=1
             elif ("!" in target and "ECLI" in target) :
+                printList.append(row[2])
                 multiple_count1+=1
             elif ('!' not in target and target.count("ECLI") > 1) :
                 multiple_count2+=1
@@ -79,8 +81,8 @@ def main () :
             else :
                 else_count+=1
     print("Frequencies of references vs. court", Counter(courts).most_common())
-    print("Frequencies tilde anchor texts: ",Counter(tildes).most_common())
     printStats(normal_count,exclamationMark_count,multiple_count1,multiple_count2,empty_count,tilde_count,other_count,total_count,else_count)
     writeToCSV(new_rows)
+    print(Counter(printList).most_common(100))
 
 main()
